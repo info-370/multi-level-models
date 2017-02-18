9b3efd95aed3ae3b4849a5cecd9187ee4c3c61a6
mkfreeman <michael.k.freeman@gmail.com>
2017-02-18 15:53:15 -0800

    Added graphics and interpretation section

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# multi-level-models
-Multi-level models for nested data
+# Multi-level Models
+This learning module introduces **Multi-level models** as a method for modeling _nested data_. Because GitHub Markdown [doesn't support](https://github.com/github/markup/issues/897) rendering equations, the module is in the [module.pdf](module.pdf) document, which was generated using the [module.Rmd](module.Rmd) file.
+
+To execute a simplified example of the analysis discussed throughout the module, run the [faculty-data.R](faculty-data.R) file.
--- faculty-data.R
+++ faculty-data.R
@@ -4,13 +4,12 @@
 library(dplyr) # data wrangling
 library(lme4) # modeling
 library(ggplot2) # visualization
+library(arm) # standard errors
 
-# Parameters
+# Parameters for generating faculty salary data
 departments <- c('sociology', 'biology', 'english', 'informatics', 'statistics')
 base.salaries <- c(40000, 50000, 60000, 70000, 80000)
-constant.base <- mean(base.salaries)
 annual.raises <- c(2000, 500, 500, 1700, 500)
-constant.raise <- mean(annual.raises)
 faculty.per.dept <- 20
 total.faculty <- faculty.per.dept * length(departments)
 
@@ -22,43 +21,48 @@ bases <- rep(base.salaries, faculty.per.dept) * runif(total.faculty, .9, 1.1) # 
 raises <- rep(annual.raises, faculty.per.dept) * runif(total.faculty, .9, 1.1) # noise
 df <- data.frame(ids, department, bases, experience, raises)
 
-# Generate random salaries: constant base and raise (no diff. by department)
+# Generate salaries (base + experience * raise)
 df <- df %>% mutate(
-            constant.salary = constant.base + experience * constant.raise, 
-            varying.base = bases + experience * constant.raise, 
-            varying.raise = constant.base + experience * raises, 
-            varying.base.raise = bases + experience * raises
-            )
+    salary = bases + experience * raises
+)
 
 # Model with varying intercept
-m1 <- lmer(varying.base.raise ~ experience + (1|department), data = df)
+m1 <- lmer(salary ~ experience + (1|department), data = df)
 df$random.intercpet.preds <- predict(m1)
 
 # Model with varying slope
-m2 <- lmer(varying.base.raise ~ experience + (0 + experience|department), data=df)
+m2 <- lmer(salary ~ experience + (0 + experience|department), data=df)
 df$random.slope.preds <- predict(m2)
 
 # Model with varying slope and intercept
-m3 <- lmer(varying.base.raise ~ experience + (1 + experience|department), data=df)
+m3 <- lmer(salary ~ experience + (1 + experience|department), data=df)
 df$random.slope.int.preds <- predict(m3)
 
 # Visualize random intercept
-ggplot(data=df, aes(x=experience, y=random.intercpet.preds, group = department, colour = department)) +
+vary.int.graph <- ggplot(data=df, aes(x=experience, y=random.intercpet.preds, group = department, colour = department)) +
   geom_line() + 
   labs(x="Experience", y="Predicted Salary") +
   ggtitle("Varying Intercept Salary Prediction") + 
   scale_colour_discrete('Department')     
 
 # Visualize random slope
-ggplot(data=df, aes(x=experience, y=random.slope.preds, group = department, colour = department)) +
+vary.slope.graph <- ggplot(data=df, aes(x=experience, y=random.slope.preds, group = department, colour = department)) +
   geom_line() + 
   labs(x="Experience", y="Predicted Salary") +
   ggtitle("Varying Slope Salary Prediction") + 
   scale_colour_discrete('Department')
 
 # Visualize random slope + intercept
-ggplot(data=df, aes(x=experience, y=random.slope.int.preds, group = department, colour = department)) +
+vary.slope.int.graph <- ggplot(data=df, aes(x=experience, y=random.slope.int.preds, group = department, colour = department)) +
   geom_line() + 
+  labs(x="Experience", y="Predicted Salary") +
+  ggtitle("Varying Slope and Intercept Salary Prediction") + 
+  scale_colour_discrete('Department')
+
+# Visualize with data
+model.with.data <- ggplot(data=df, aes(x=experience, y=random.slope.int.preds, group = department, colour = department)) +
+  geom_line() + 
+  geom_point(aes(x=experience, y=salary, group = department, colour = department)) +
   labs(x="Experience", y="Predicted Salary") +
   ggtitle("Varying Slope and Intercept Salary Prediction") + 
   scale_colour_discrete('Department')
